add7d7639da90a36694add422aae0bce2614d14a
alibekshak <alibekshakiro1@gmail.com>
2023-08-25 18:12:51 +0600

    working_table_view

---
+++ Contact/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by Apple on 25.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell: UITableViewCell
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "MyCell"){
+            print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+            cell = reuseCell
+        }else{
+            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            cell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+        }
+         configure(cell: &cell, for: indexPath)
+        return cell
+    }
+    
+    private func configure(cell: inout UITableViewCell, for indexPath: IndexPath) {
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Сторка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+    }
+}
